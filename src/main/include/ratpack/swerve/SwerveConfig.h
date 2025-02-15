a225bd4a30f77a1e39794682bf72ae4a6b29b026
Mohan Kartha <Mckartha@gmail.com>
2025-02-14 21:32:07 -0500

    Updated SwerveConfig.h with SparkMAX Controller CAN IDs for new Drivetrain (#10)
    Co-authored-by: Student <ratpack-student@gmail.com>

--- src/main/include/ratpack/swerve/SwerveConfig.h
+++ src/main/include/ratpack/swerve/SwerveConfig.h
@@ -10,14 +10,14 @@
 // #                  Front Left Module                      #
 // ###########################################################
 // --------------------Absolute Encoder-----------------------
-const int FL_ABS_ENC_PORT = 9;
+const int FL_ABS_ENC_PORT = 3;
 const bool FL_ABS_ENC_INVERTED = false;
 const frc::Rotation2d FL_ZERO_HEADING{units::degree_t{358.59f}};
 
 ctre::phoenix6::hardware::CANcoder fl_abs_enc{FL_ABS_ENC_PORT};
 
 // -----------------------Turn Motor--------------------------
-const int FL_TURN_MTR_ID = 16;
+const int FL_TURN_MTR_ID = 15;
 const bool FL_TURN_MTR_INVERTED = true;
 
 rev::spark::SparkMax fl_turn_mtr{FL_TURN_MTR_ID, rev::spark::SparkMax::MotorType::kBrushless};
@@ -25,7 +25,7 @@ rev::spark::SparkRelativeEncoder fl_turn_enc = fl_turn_mtr.GetEncoder();
 rev::spark::SparkClosedLoopController fl_turn_pid = fl_turn_mtr.GetClosedLoopController();
 
 // -----------------------Drive Motor-------------------------
-const int FL_DRIVE_MTR_ID = 7;
+const int FL_DRIVE_MTR_ID = 18;
 const double FL_POSITION_CORRECTION_FACTOR = 1.0;
 
 rev::spark::SparkMax fl_drive_mtr{FL_DRIVE_MTR_ID, rev::spark::SparkMax::MotorType::kBrushless};
@@ -37,14 +37,14 @@ rev::spark::SparkClosedLoopController fl_drive_pid = fl_drive_mtr.GetClosedLoopC
 // #                  Front Right Module                     #
 // ###########################################################
 // --------------------Absolute Encoder-----------------------
-const int FR_ABS_ENC_PORT = 3;
+const int FR_ABS_ENC_PORT = 6;
 const bool FR_ABS_ENC_INVERTED = false;
 const frc::Rotation2d FR_ZERO_HEADING{units::degree_t{359.12f}};
 
 ctre::phoenix6::hardware::CANcoder fr_abs_enc{FR_ABS_ENC_PORT};
 
 // -----------------------Turn Motor--------------------------
-const int FR_TURN_MTR_ID = 5;
+const int FR_TURN_MTR_ID = 7;
 const bool FR_TURN_MTR_INVERTED = true;
 
 rev::spark::SparkMax fr_turn_mtr{FR_TURN_MTR_ID, rev::spark::SparkMax::MotorType::kBrushless};
@@ -52,7 +52,7 @@ rev::spark::SparkRelativeEncoder fr_turn_enc = fr_turn_mtr.GetEncoder();
 rev::spark::SparkClosedLoopController fr_turn_pid = fr_turn_mtr.GetClosedLoopController();
 
 // -----------------------Drive Motor-------------------------
-const int FR_DRIVE_MTR_ID = 10;
+const int FR_DRIVE_MTR_ID = 8;
 const double FR_POSITION_CORRECTION_FACTOR = 1.0;
 
 rev::spark::SparkMax fr_drive_mtr{FR_DRIVE_MTR_ID, rev::spark::SparkMax::MotorType::kBrushless};
@@ -72,7 +72,7 @@ ctre::phoenix6::hardware::CANcoder bl_abs_enc{BL_ABS_ENC_PORT};
 
 
 // -----------------------Turn Motor--------------------------
-const int BL_TURN_MTR_ID = 8;
+const int BL_TURN_MTR_ID = 17;
 const bool BL_TURN_MTR_INVERTED = true;
 
 rev::spark::SparkMax bl_turn_mtr{BL_TURN_MTR_ID, rev::spark::SparkMax::MotorType::kBrushless};
@@ -80,7 +80,7 @@ rev::spark::SparkRelativeEncoder bl_turn_enc = bl_turn_mtr.GetEncoder();
 rev::spark::SparkClosedLoopController bl_turn_pid = bl_turn_mtr.GetClosedLoopController();
 
 // -----------------------Drive Motor-------------------------
-const int BL_DRIVE_MTR_ID = 17;
+const int BL_DRIVE_MTR_ID = 19;
 const double BL_POSITION_CORRECTION_FACTOR = 1.0;
 
 rev::spark::SparkMax bl_drive_mtr{BL_DRIVE_MTR_ID, rev::spark::SparkMax::MotorType::kBrushless};
@@ -92,14 +92,14 @@ rev::spark::SparkClosedLoopController bl_drive_pid = bl_drive_mtr.GetClosedLoopC
 // #                  Back Right Module                      #
 // ###########################################################
 // --------------------Absolute Encoder-----------------------
-const int BR_ABS_ENC_PORT = 6;
+const int BR_ABS_ENC_PORT = 9;
 const bool BR_ABS_ENC_INVERTED = false;
 const frc::Rotation2d BR_ZERO_HEADING{units::degree_t{0.26f}};
 
 ctre::phoenix6::hardware::CANcoder br_abs_enc{BR_ABS_ENC_PORT};
 
 // -----------------------Turn Motor--------------------------
-const int BR_TURN_MTR_ID = 1;
+const int BR_TURN_MTR_ID = 2;
 const bool BR_TURN_MTR_INVERTED = true;
 
 rev::spark::SparkMax br_turn_mtr{BR_TURN_MTR_ID, rev::spark::SparkMax::MotorType::kBrushless};
@@ -107,7 +107,7 @@ rev::spark::SparkRelativeEncoder br_turn_enc = br_turn_mtr.GetEncoder();
 rev::spark::SparkClosedLoopController br_turn_pid = br_turn_mtr.GetClosedLoopController();
 
 // -----------------------Drive Motor-------------------------
-const int BR_DRIVE_MTR_ID = 2;
+const int BR_DRIVE_MTR_ID = 20;
 const double BR_POSITION_CORRECTION_FACTOR = 1.0;
 
 rev::spark::SparkMax br_drive_mtr{BR_DRIVE_MTR_ID, rev::spark::SparkMax::MotorType::kBrushless};
