25f3598d7545d2a4f45f6677908ad4a41b4ce389
Harsh-Sinha <harsh.sinha@hotmail.com>
2025-03-01 14:30:16 -0500

    update mech config based on tuning

--- src/main/include/MechanismConfig.h
+++ src/main/include/MechanismConfig.h
@@ -13,19 +13,19 @@ namespace ratbot
     {
         namespace Flywheel
         {
-            const double P = 0.0;
+            const double P = 0.00025;
             const double I = 0.0;
-            const double D = 0.0;
+            const double D = 0.35;
             const double F = 0.0;
-            const double VEL_CONV_FACTOR = 0.0;
-            const double CURRENT_LIM = 0.0;
+            const double VEL_CONV_FACTOR = 1.0;
+            const double CURRENT_LIM = 30.0;
             const bool INVERTED = false;
             rev::spark::SparkBaseConfig::IdleMode IDLE_MODE = rev::spark::SparkBaseConfig::IdleMode::kCoast;
         }
 
         namespace Indexer
         {
-            const double CURRENT_LIM = 0.0;
+            const double CURRENT_LIM = 20.0;
             const bool INVERTED = false;
             rev::spark::SparkBaseConfig::IdleMode IDLE_MODE = rev::spark::SparkBaseConfig::IdleMode::kCoast;
         } 
